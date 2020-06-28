a4aa976d0d5395c558a53f79ed2712119fcaa904
Jonny Shipton <jonny@myrtlesoftware.com>
2020-06-28 22:41:22 +0100

    Pin nixpkgs to 20.03 and mention cachix in README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # clash-basement
 
 Experiments with [Clash](https://clash-lang.org/)
+
+
+## Cachix
+
+The nix-shell can take a while to build, so it's cached on [cachix](https://cachix.org).
--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,11 @@
 {}:
 
 let
-  # TODO: pin 20.03
-  nixpkgs-src = <nixpkgs>;
+  nixpkgs-rev = "5272327b81ed355bbed5659b8d303cf2979b6953"; # 20.03
+  nixpkgs-src = builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs-channels/archive/${nixpkgs-rev}.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  };
 
 in
 
